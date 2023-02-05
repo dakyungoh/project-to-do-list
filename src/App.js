5e0320e9caff31c3bea24662c343177ea9a6a79b
dakyungoh <dakyung.work@gmail.com>
2023-02-06 01:53:44 +0900

    체크박스 적용하기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([
     "산책하기",
     "밥먹기",
     "공부하기",
     "잠 자기",
   ]);
-  const [newTodo, setNewTodo] = useState("");
+  const [isChecked, setIsChecked] = useState([false, false, false, false]);
 
   function onClickAddButton() {
     setTodos([...todos, newTodo]);
+    setIsChecked([...isChecked, false]);
     setNewTodo("");
   }
 
@@ -19,6 +21,12 @@ function App() {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
+  }
+
+  function checkboxButton(event, index) {
+    const newIsChecked = [...isChecked];
+    newIsChecked[index] = event.target.checked;
+    setIsChecked(newIsChecked);
   }
 
   return (
@@ -41,8 +49,18 @@ function App() {
         <ul>
           {todos.map((todo, index) => (
             <li key={index}>
-              <input className="todo-item-checkbox" type="checkbox" />
-              <span className="todo-item-text">{todo}</span>
+              <input
+                className="todo-item-checkbox"
+                type="checkbox"
+                checked={isChecked[index]}
+                onClick={(event) => checkboxButton(event, index)}
+              />
+              {isChecked[index] === false ? (
+                <span className="todo-item-text">{todo}</span>
+              ) : (
+                <span className="todo-item-text-line-through">{todo}</span>
+              )}
+
               <button
                 className="delete-button"
                 onClick={() => {
