ede034efdf4d4c8446ea7ba57a76510d9e47fecb
dakyungoh <dakyung.work@gmail.com>
2023-02-05 22:47:10 +0900

    todo item 추가 및 삭제 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,22 +8,49 @@ function App() {
     "공부하기",
     "잠 자기",
   ]);
+  const [newTodo, setNewTodo] = useState("");
+
+  function onClickAddButton() {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  }
+
+  function onClickDeleteButton(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
 
   return (
     <div className="App">
       <h1>To Do List</h1>
       <div className="todo-input-box">
-        <input type="text" placeholder="내용을 입력하세요." />
-        <button className="todo-input-button">+</button>
+        <input
+          type="text"
+          placeholder="내용을 입력하세요."
+          value={newTodo}
+          onChange={(event) => {
+            setNewTodo(event.target.value);
+          }}
+        />
+        <button className="todo-input-button" onClick={onClickAddButton}>
+          +
+        </button>
       </div>
       <div className="todo-list">
         <ul>
-          {todos.map((todo) => (
-            <li>
+          {todos.map((todo, index) => (
+            <li key={index}>
               <input className="todo-item-checkbox" type="checkbox" />
               <span className="todo-item-text">{todo}</span>
-              <button className="insert-button">수정</button>
-              <button className="delete-button">지우기</button>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  onClickDeleteButton(index);
+                }}
+              >
+                지우기
+              </button>
             </li>
           ))}
         </ul>
